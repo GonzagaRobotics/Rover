5fd41b06d0077666ccdc651ce506d3011b499c43
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2025-05-20 16:41:04 -0700

    New core interfaces, created boilerplate for others

---
+++ src/interfaces/core_interfaces/include/core_interfaces/Types.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <memory>
+#include "rclcpp/rclcpp.hpp"
+#include "core_interfaces/msg/heartbeat.hpp"
+#include "core_interfaces/msg/heartbeat_disconnect.hpp"
+#include "core_interfaces/srv/heartbeat_connect.hpp"
+
+struct Heartbeat
+{
+    using SharedPtr = std::shared_ptr<Heartbeat>;
+
+    rclcpp::Time stamp;
+    uint32_t id;
+};
+
+template <>
+struct rclcpp::TypeAdapter<Heartbeat, core_interfaces::msg::Heartbeat>
+{
+    using is_specialized = std::true_type;
+    using custom_type = Heartbeat;
+    using ros_message_type = core_interfaces::msg::Heartbeat;
+
+    static void convert_to_ros_message(const custom_type &source, ros_message_type &destination)
+    {
+        destination.stamp = source.stamp;
+        destination.id = source.id;
+    }
+
+    static void convert_to_custom_message(const ros_message_type &source, custom_type &destination)
+    {
+        destination.stamp = source.stamp;
+        destination.id = source.id;
+    }
+};
+
+RCLCPP_USING_CUSTOM_TYPE_AS_ROS_MESSAGE_TYPE(Heartbeat, core_interfaces::msg::Heartbeat);
+
+using HeartbeatDisconnect = core_interfaces::msg::HeartbeatDisconnect;
+
+using HeartbeatConnect = core_interfaces::srv::HeartbeatConnect;
