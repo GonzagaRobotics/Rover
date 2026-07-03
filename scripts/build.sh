81b8cbe207f346c54490a47eb76691843a61a691
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-07-02 20:04:29 -0700

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,22 @@
 
 # Use inside the first level of the Rover directory
 
+CMD="colcon build --symlink-install"
+
 case "$1" in
     "" )
-        colcon build --symlink-install
         ;;
     * )
-        colcon build --symlink-install --packages-up-to $*
+        CMD="$CMD --packages-up-to $1"
         ;;
 esac
+
+case "$2" in
+    "" )
+        ;;
+    * )
+        CMD="$CMD --cmake-args -DCMAKE_BUILD_TYPE=$2"
+        ;;
+esac
+
+$CMD
