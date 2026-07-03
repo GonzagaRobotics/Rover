74c0327116a011326af4bba5d88462792c038b57
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-07-02 19:43:32 -0700

    init coarse package

---
+++ src/auto/coarse/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'coarse'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='damon',
+    maintainer_email='57426668+BruhSoundEffectNumber2@users.noreply.github.com',
+    description='Coarse layer of autonomy.',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
