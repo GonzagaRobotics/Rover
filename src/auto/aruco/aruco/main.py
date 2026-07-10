dbfbe2a29e1ea1e9a0366c2f63c8757bd1af662c
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-07-10 11:00:37 -0700

    Aruco uses cv 4.6, fix package, other improvements for consistency

--- src/auto/aruco/aruco/main.py
+++ src/auto/aruco/aruco/main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import cv2 as cv
+import cv2
 import rclpy
 from rclpy.node import Node, Parameter, SetParametersResult
 from sensor_msgs.msg import Image, RegionOfInterest, CameraInfo
@@ -8,21 +8,47 @@ from visualization_msgs.msg import Marker, MarkerArray
 from auto_msgs.msg import Aruco as ArucoMsg
 
 
-class Aruco(Node):
+class ArucoDetector:
+    def __init__(self, marker_size: float):
+        self.params = cv2.aruco.DetectorParameters_create()
+        self.params.cornerRefinementMethod = cv2.aruco.CORNER_REFINE_SUBPIX
+
+        self.ar_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_4X4_50)
+
+        self.object_points = self._object_points(marker_size)
+
+    def set_marker_size(self, marker_size: float):
+        self.object_points = self._object_points(marker_size)
+
+    def detect(self, img: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
+        corners, ids, _ = cv2.aruco.detectMarkers(img, self.ar_dict, parameters=self.params)
+
+        return corners, ids
+
+    def _object_points(self, marker_size: float) -> np.ndarray:
+        half = marker_size / 2
+
+        return np.array([
+            [-half, -half, 0],
+            [half, -half, 0],
+            [half, half, 0],
+            [-half, half, 0]],
+            dtype=np.float32)
+
+
+class ArucoNode(Node):
     def __init__(self):
-        super().__init__("aruco_node")
+        super().__init__("aruco_node", namespace="auto")
 
         self._ready = False
 
-        self.marker_size = self.declare_parameter("marker_size", 0.15).value
-
-        self._setup_marker_points()
+        marker_size = self.declare_parameter("marker_size", 0.15).value
 
         self._last_img = None
-        self._detector = self._make_detector()
+        self._detector = ArucoDetector(marker_size)
 
-        self._viz_pub = self.create_publisher(MarkerArray, '/aruco/viz', 10)
-        self._aruco_pub = self.create_publisher(ArucoMsg, "/aruco/detect", 10)
+        self._viz_pub = self.create_publisher(MarkerArray, 'aruco/viz', 10)
+        self._aruco_pub = self.create_publisher(ArucoMsg, "aruco/detect", 10)
 
         self.create_subscription(Image, "/vision/main/image_rect_color", self.cam_cb, 1)
         self.create_subscription(CameraInfo, "/vision/main/camera_info", self.cam_cb, 1)
@@ -37,8 +63,7 @@ class Aruco(Node):
                 if param.value <= 0:
                     return SetParametersResult(successful=False, reason="Marker size must be > 0.")
 
-                self.marker_size = param.value
-                self._setup_marker_points()
+                self._detector.set_marker_size(param.value)
                 return SetParametersResult(successful=True)
 
     def cam_cb(self, msg: Image | CameraInfo):
@@ -59,7 +84,7 @@ class Aruco(Node):
         img = np.frombuffer(self._last_img.data, dtype=np.uint8).reshape(
             self._last_img.height, self._last_img.width, -1)
 
-        corners, ids, _ = self._detector.detectMarkers(img)
+        corners, ids = self._detector.detect(img)
 
         msg = ArucoMsg()
         msg.header.frame_id = "map"
@@ -71,7 +96,7 @@ class Aruco(Node):
                 continue
 
             image_points = corners[i].reshape(-1, 2).astype(np.float32)
-            _, _, t = cv.solvePnP(self._object_points, image_points, self._cam_mtx, self._dist_coeffs)
+            _, _, t = cv2.solvePnP(self._detector.object_points, image_points, self._cam_mtx, self._dist_coeffs)
 
             roi = RegionOfInterest()
             roi.x_offset = int(image_points[:, 0].min())
@@ -103,29 +128,11 @@ class Aruco(Node):
 
         self._last_img = None
 
-    def _make_detector(self) -> cv.aruco.ArucoDetector:
-        parameters = cv.aruco.DetectorParameters()
-        parameters.cornerRefinementMethod = cv.aruco.CORNER_REFINE_SUBPIX
-
-        aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_50)
-
-        return cv.aruco.ArucoDetector(aruco_dict, parameters)
-
-    def _setup_marker_points(self):
-        half = self.marker_size / 2
-
-        self._object_points = np.array([
-            [-half, -half, 0],
-            [half, -half, 0],
-            [half, half, 0],
-            [-half, half, 0]],
-            dtype=np.float32)
-
 
 def main(args=None):
     rclpy.init(args=args)
 
-    node = Aruco()
+    node = ArucoNode()
 
     try:
         rclpy.spin(node)
