32c5c0b1c7d20fa7405fa99a17b6c1c54ea5a6de
Damon <57426668+BruhSoundEffectNumber2@users.noreply.github.com>
2026-08-18 09:59:01 -0700

    Use new serial system with arm, correct some changes

--- src/arm/arm/main.py
+++ src/arm/arm/main.py
@@ -1,19 +1,19 @@
+import time
 import serial
 import rclpy
 from rclpy.node import Node
+from rclpy.logging import get_logger
 from std_msgs.msg import Float32, Int32, Bool
 
-TARGET_NAME = '/dev/ttyCH341USB0'
 
 class Arm(Node):
+    _ser = None
+
     def __init__(self):
         super().__init__('arm')
 
-        try:
-            self._ser = serial.Serial(TARGET_NAME, baudrate=115200)
-        except serial.SerialException as e:
-            self.get_logger().error(f'Failed to open serial port: {e}')
-            raise
+        ser_root = self.declare_parameter("ser_root", "/dev/ttyUSB").value
+        self._ser = self.find_serial(ser_root)
 
         # These subscriptions follow the command order specified by the arm
         self.create_subscription(Float32, 'arm/base', self.base_cb, 10)
@@ -22,10 +22,31 @@ class Arm(Node):
         self.create_subscription(Float32, 'arm/wrist', self.wrist_cb, 10)
         self.create_subscription(Int32, 'arm/minor/x', self.minor_x_cb, 10)
         self.create_subscription(Int32, 'arm/minor/rot', self.minor_rot_cb, 10)
-        self.create_subscription(Bool, 'arm/minor/grab/red', self.grab_red_cb, 10)
-        self.create_subscription(Bool, 'arm/minor/grab/blk', self.grab_blk_cb, 10)
+        self.create_subscription(Bool, 'arm/minor/grab', self.minor_grab_cb, 10)
 
-        self.get_logger().info('Ready')
+    def __del__(self):
+        if self._ser is not None:
+            self._ser.close()
+
+    def find_serial(self, root: str) -> serial.Serial:
+        for i in range(10):
+            try:
+                ser = serial.Serial(f"{root}{i}", 115200, timeout=1)
+            except serial.SerialException:
+                continue
+
+            # The microcontroller needs time to reset
+            time.sleep(2)
+
+            ser.write(bytes([0xff, 0x00]))
+            if ser.readline() != b"ARM\n":
+                ser.close()
+                continue
+
+            self.get_logger().info(f"Found arm at {ser.name}")
+            return ser
+
+        raise serial.SerialException(f"Failed to find a valid arm within {root}")
 
     def base_cb(self, msg: Float32):
         self._send_dc(0, msg.data)
@@ -46,15 +67,12 @@ class Arm(Node):
         # TODO: Limit angles
         self._send_stepper_servo(5, msg.data)
 
-    def grab_red_cb(self, msg: Bool):
-        self._send_stepper_servo(6, 180 if msg.data else 0)
-
-    def grab_blk_cb(self, msg: Bool):
-        self._send_stepper_servo(7, 180 if msg.data else 0)
+    def minor_grab_cb(self, msg: Bool):
+        self._send_stepper_servo(6, 180 if msg.data else 90)
 
     def _send_dc(self, cmd: int, val: float):
         # Byte order: [command, direction, speed]
-        direction = 1 if val >= 0 else 0
+        direction = 0 if val >= 0 else 1
         speed = int(min(abs(val) * 255, 255))
 
         buf = bytes([cmd, direction, speed])
@@ -70,11 +88,14 @@ class Arm(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    arm = Arm()
-    
+
     try:
-        rclpy.spin(arm)
+        node = Arm()
+    except Exception as exc:
+        get_logger("global").error(str(exc))
+        return
+
+    try:
+        rclpy.spin(node)
     except KeyboardInterrupt:
         pass
-    finally:
-        arm._ser.close()
